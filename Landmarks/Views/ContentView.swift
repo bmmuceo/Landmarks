0870a89678ec284cc1dac3717835d9c0cc083cb3
bmmuceo <bmmuceo@gmail.com>
2025-04-08 02:22:42 -0400

    Landmarks: Updated

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by RyanA on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+#Preview {
+    ContentView()
+}
