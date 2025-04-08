272194006fade83f4d7303af840bf28b908b34fe
bmmuceo <bmmuceo@gmail.com>
2025-04-07 22:12:39 -0400

    Landmarks: Initial Comm

---
+++ Landmarks/ CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//   CircleImage.swift
+//  Landmarks
+//
+//  Created by RyanA on 4/7/25.
+//
+
+import SwiftUI
+
+struct _CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _CircleImage()
+}
